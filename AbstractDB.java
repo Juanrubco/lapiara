5d8d892aa990d62b2ab46bb4afb37ee9b7eb3d2c
Juanrubco <48248843+Juanrubco@users.noreply.github.com>
2020-05-10 15:07:55 -0500

    Add files via upload

---
+++ AbstractDB.java
@@ -0,0 +1,15 @@
+package servlet
+
+import java.sql.Connection;
+
+
+public abstract class AbstractDB 
+{
+    protected Connection conn;
+    
+    public AbstractDB()
+    {
+        ConexionDB x = new ConexionDB();
+        this.conn = x.getConn();
+    }
+}
---
+++ GestionPersona.java
@@ -0,0 +1,59 @@
+package servlet
+
+import AbstractDB;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import Persona;
+
+public class GestionPersona extends AbstractDB
+{
+
+	public GestionPersona()
+	{
+		super();
+	}
+	public static Persona Login(String user String pass)
+	{
+		String sql="select*from persona where usuario=? and pass=?";
+		ResultSet rst;
+		Usuario usuario=null;
+		try
+		{
+			Connection conn = ConexionDB.getConn();
+			PreparedStatement pstm=this.conn.prepareStatement(sql);
+			pstm.setString(1,user);
+			pstm.setString(2,pass);
+			rst=pstm.excuteQuery();
+
+			while(rst.next())
+			{
+				usuario=new Usuario();
+				usuario.setID(rst.getString("idpersona"));
+				usuario.setNombre(rst.getString("nombre"));
+				usuario.setApellido(rst.getString("apellido"));
+				usuario.setCargo(rst.getString("cargo"));
+				usuario.setUser(rst.getString("ususario"));
+				usuario.setPass(rst.getString("pass"));
+			}
+			rst.close();
+
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e);
+		}
+		return usuario;
+	}
+	public void cierraConexion() 
+	{
+        try 
+        {
+            this.conn.close();
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+    }
+}
