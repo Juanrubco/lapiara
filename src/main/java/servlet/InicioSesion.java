11849f05a7cc0178819c2a429b496989af63e06c
Juanrubco <48248843+Juanrubco@users.noreply.github.com>
2020-05-10 15:27:12 -0500

    Add files via upload

--- src/main/java/servlet/InicioSesion.java
+++ src/main/java/servlet/InicioSesion.java
@@ -10,70 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "Login", 
+        name = "InicioSesion", 
         urlPatterns = {"/InicioSesion"}
     )
-public class InicioSesion extends HttpServlet{
+public class InicioSesion extends HttpServlet
+{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException 
-            {
-        MySingleton tempo = MySingleton.laconstructora();
-        String valor = req.getParameter("2");
-        if (valor.equals("com")) 
-        { 
-            if (tempo.inicioComprador(req.getParameter("IC"),req.getParameter("IP"))) 
-            {
+     {
+  
+                 resp.sendRedirect("index.html") 
+
+
+
             
-                String datos = "Inicio Sesion como comprador";
-                resp.sendRedirect("MenuC.html");
-            	  ServletOutputStream out = resp.getOutputStream();
-                  out.write(datos.getBytes());
-
-
-                	 out.flush();
-        			out.close();
-                }
-                else
-                {
-                     String datos = "Contraseña o correo incorrectos";
-                  ServletOutputStream out = resp.getOutputStream();
-                  out.write(datos.getBytes());
-
-
-                     out.flush();
-                    out.close();
-                }
-            }
-            else if (valor.equals("ven")) 
-            {
-                if (tempo.inicioVendedor(req.getParameter("IC"), req.getParameter("IP"))) 
-                {
-                    String datos = "Inicio Sesion como vendedor";
-                    resp.sendRedirect("MenuV.html");
-                  ServletOutputStream out = resp.getOutputStream();
-                  out.write(datos.getBytes());
-
-
-                     out.flush();
-                    out.close();
-                
-            }
-            else
-            {
-                 String datos = "Contraseña o correo incorrectos";
-                  ServletOutputStream out = resp.getOutputStream();
-                  out.write(datos.getBytes());
-
-
-                     out.flush();
-                    out.close();
-
-            }
             	
             	
-            }
+            
 
-}
+
+      }
 }
